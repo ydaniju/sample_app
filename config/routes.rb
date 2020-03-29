7eee00440c08e0b46d9c4b980557208dfbab31c0
Yusuf Daniju <danijuyusuf@outlook.com>
2020-03-29 23:17:00 +0100

    complete mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root 'static_pages#home'
+
+  get 'home', to: 'static_pages#home', as: :home
+  get 'about', to: 'static_pages#about', as: :about
+  get 'help', to: 'static_pages#help', as: :help
+  get 'contact', to: 'static_pages#contact', as: :contact
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,37 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
-    get static_pages_home_url
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test 'should get root' do
+    get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test 'should get home' do
+    get home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test 'should get about' do
+    get about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test 'should get contact' do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
