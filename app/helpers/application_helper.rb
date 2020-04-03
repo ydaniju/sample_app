346a9e28b03c4d920d481d92ec7b47e21f3af78b
Yusuf Daniju <danijuyusuf@outlook.com>
2020-04-04 00:14:52 +0100

    move all to slim

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def full_title(page_title = "")
-    base_title = "Ruby on Rails Tutorial Sample App"
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
 
     if page_title.empty?
       base_title
